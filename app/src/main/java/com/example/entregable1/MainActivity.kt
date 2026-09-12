11dcbc5d42a873cbd136412649982a14ac93043f
jeko <jericodiazr@gmail.com>
2026-09-12 13:06:35 -0500

    gestion de conexion  SQLite

--- app/src/main/java/com/example/entregable1/MainActivity.kt
+++ app/src/main/java/com/example/entregable1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.entregable1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val listViewVentas = findViewById<ListView>(R.id.listViewVentas)
+        val dbHelper = DatabaseHelper(this)
+        val listaVentasTexto = ArrayList<String>()
+        val cursor = dbHelper.obtenerTodasLasVentas()
+
+        if (cursor.moveToFirst()){
+            do {
+                val codigo = cursor.getString(cursor.getColumnIndexOrThrow("codigo"))
+                val nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"))
+                val precio = cursor.getDouble(cursor.getColumnIndexOrThrow("precio"))
+                val cantidad = cursor.getInt(cursor.getColumnIndexOrThrow("cantidad"))
+                val tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"))
+                val fecha = cursor.getString(cursor.getColumnIndexOrThrow("fecha"))
+                val item = "[$codigo] $nombre\nPrecio: S/$precio | Cant: $cantidad | Tipo: $tipo\nFecha: $fecha"
+                listaVentasTexto.add(item)
+            }while (cursor.moveToNext())
+        }else {
+            Toast.makeText(this, "No se encontraron ventas", Toast.LENGTH_LONG).show()
+        }
+        cursor.close()
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaVentasTexto)
+        listViewVentas.adapter = adapter
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
